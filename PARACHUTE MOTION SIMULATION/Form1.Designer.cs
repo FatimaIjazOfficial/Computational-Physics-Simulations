1f18e5baeeffc7942247e5ad73974fe39159270e
FatimaIjazOfficial <fatimaijaz.offical@gmail.com>
2026-07-31 04:28:19 +0500

    adding simulation of parachute

---
+++ PARACHUTE MOTION SIMULATION/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace PARACHUTE_MOTION_SIMULATION
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.panelParameters = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelVelocity = new System.Windows.Forms.Label();
+            this.txtVelocity = new System.Windows.Forms.TextBox();
+            this.labelGravity = new System.Windows.Forms.Label();
+            this.txtGravity = new System.Windows.Forms.TextBox();
+            this.labelDrag = new System.Windows.Forms.Label();
+            this.txtDrag = new System.Windows.Forms.TextBox();
+            this.labelTimeStep = new System.Windows.Forms.Label();
+            this.txtTimeStep = new System.Windows.Forms.TextBox();
+            this.labelSteps = new System.Windows.Forms.Label();
+            this.txtSteps = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.picGraph = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            this.panelParameters.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.BurlyWood;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(900, 30);
+            this.toolStrip1.TabIndex = 2;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold);
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(65, 27);
+            this.toolStripButton1.Text = "▶ Run";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Font = this.toolStripButton1.Font;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(84, 27);
+            this.toolStripButton2.Text = "⏹ Clear";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Font = this.toolStripButton1.Font;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(86, 27);
+            this.toolStripButton3.Text = "🔄 Reset";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Font = this.toolStripButton1.Font;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(78, 27);
+            this.toolStripButton4.Text = "💾 Save";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.Font = this.toolStripButton1.Font;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(131, 27);
+            this.toolStripButton5.Text = "📊 Export CSV";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // panelParameters
+            // 
+            this.panelParameters.BackColor = System.Drawing.Color.AliceBlue;
+            this.panelParameters.Controls.Add(this.labelTitle);
+            this.panelParameters.Controls.Add(this.labelVelocity);
+            this.panelParameters.Controls.Add(this.txtVelocity);
+            this.panelParameters.Controls.Add(this.labelGravity);
+            this.panelParameters.Controls.Add(this.txtGravity);
+            this.panelParameters.Controls.Add(this.labelDrag);
+            this.panelParameters.Controls.Add(this.txtDrag);
+            this.panelParameters.Controls.Add(this.labelTimeStep);
+            this.panelParameters.Controls.Add(this.txtTimeStep);
+            this.panelParameters.Controls.Add(this.labelSteps);
+            this.panelParameters.Controls.Add(this.txtSteps);
+            this.panelParameters.Controls.Add(this.statusStrip1);
+            this.panelParameters.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelParameters.Location = new System.Drawing.Point(0, 30);
+            this.panelParameters.Name = "panelParameters";
+            this.panelParameters.Size = new System.Drawing.Size(260, 470);
+            this.panelParameters.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("News706 BT", 14F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.Location = new System.Drawing.Point(15, 20);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(227, 22);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Simulation Parameters";
+            // 
+            // labelVelocity
+            // 
+            this.labelVelocity.AutoSize = true;
+            this.labelVelocity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.labelVelocity.Location = new System.Drawing.Point(20, 70);
+            this.labelVelocity.Name = "labelVelocity";
+            this.labelVelocity.Size = new System.Drawing.Size(151, 17);
+            this.labelVelocity.TabIndex = 1;
+            this.labelVelocity.Text = "Initial Velocity (m/s)";
+            // 
+            // txtVelocity
+            // 
+            this.txtVelocity.Location = new System.Drawing.Point(170, 70);
+            this.txtVelocity.Name = "txtVelocity";
+            this.txtVelocity.Size = new System.Drawing.Size(70, 20);
+            this.txtVelocity.TabIndex = 2;
+            this.txtVelocity.Text = "50";
+            // 
+            // labelGravity
+            // 
+            this.labelGravity.AutoSize = true;
+            this.labelGravity.Font = this.labelVelocity.Font;
+            this.labelGravity.Location = new System.Drawing.Point(20, 110);
+            this.labelGravity.Name = "labelGravity";
+            this.labelGravity.Size = new System.Drawing.Size(108, 17);
+            this.labelGravity.TabIndex = 3;
+            this.labelGravity.Text = "Gravity (m/s²)";
+            // 
+            // txtGravity
+            // 
+            this.txtGravity.Location = new System.Drawing.Point(170, 110);
+            this.txtGravity.Name = "txtGravity";
+            this.txtGravity.Size = new System.Drawing.Size(70, 20);
+            this.txtGravity.TabIndex = 4;
+            this.txtGravity.Text = "9.8";
+            // 
+            // labelDrag
+            // 
+            this.labelDrag.AutoSize = true;
+            this.labelDrag.Font = this.labelVelocity.Font;
+            this.labelDrag.Location = new System.Drawing.Point(20, 150);
+            this.labelDrag.Name = "labelDrag";
+            this.labelDrag.Size = new System.Drawing.Size(125, 17);
+            this.labelDrag.TabIndex = 5;
+            this.labelDrag.Text = "Drag Coefficient";
+            // 
+            // txtDrag
+            // 
+            this.txtDrag.Location = new System.Drawing.Point(170, 150);
+            this.txtDrag.Name = "txtDrag";
+            this.txtDrag.Size = new System.Drawing.Size(70, 20);
+            this.txtDrag.TabIndex = 6;
+            this.txtDrag.Text = "1";
+            // 
+            // labelTimeStep
+            // 
+            this.labelTimeStep.AutoSize = true;
+            this.labelTimeStep.Font = this.labelVelocity.Font;
+            this.labelTimeStep.Location = new System.Drawing.Point(20, 190);
+            this.labelTimeStep.Name = "labelTimeStep";
+            this.labelTimeStep.Size = new System.Drawing.Size(106, 17);
+            this.labelTimeStep.TabIndex = 7;
+            this.labelTimeStep.Text = "Time Step (s)";
+            // 
+            // txtTimeStep
+            // 
+            this.txtTimeStep.Location = new System.Drawing.Point(170, 190);
+            this.txtTimeStep.Name = "txtTimeStep";
+            this.txtTimeStep.Size = new System.Drawing.Size(70, 20);
+            this.txtTimeStep.TabIndex = 8;
+            this.txtTimeStep.Text = "0.1";
+            // 
+            // labelSteps
+            // 
+            this.labelSteps.AutoSize = true;
+            this.labelSteps.Font = this.labelVelocity.Font;
+            this.labelSteps.Location = new System.Drawing.Point(20, 230);
+            this.labelSteps.Name = "labelSteps";
+            this.labelSteps.Size = new System.Drawing.Size(129, 17);
+            this.labelSteps.TabIndex = 9;
+            this.labelSteps.Text = "Number of Steps";
+            // 
+            // txtSteps
+            // 
+            this.txtSteps.Location = new System.Drawing.Point(170, 230);
+            this.txtSteps.Name = "txtSteps";
+            this.txtSteps.Size = new System.Drawing.Size(70, 20);
+            this.txtSteps.TabIndex = 10;
+            this.txtSteps.Text = "150";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 444);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(260, 26);
+            this.statusStrip1.TabIndex = 11;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(58, 21);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // picGraph
+            // 
+            this.picGraph.BackColor = System.Drawing.Color.NavajoWhite;
+            this.picGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picGraph.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picGraph.Location = new System.Drawing.Point(260, 30);
+            this.picGraph.Name = "picGraph";
+            this.picGraph.Size = new System.Drawing.Size(640, 470);
+            this.picGraph.TabIndex = 0;
+            this.picGraph.TabStop = false;
+            this.picGraph.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picGraph_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.picGraph);
+            this.Controls.Add(this.panelParameters);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Computational Physics - Parachute Velocity-Time Simulation";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panelParameters.ResumeLayout(false);
+            this.panelParameters.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //==================================================
+        // DECLARATIONS
+        //==================================================
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+
+        private System.Windows.Forms.Panel panelParameters;
+
+        private System.Windows.Forms.Label labelTitle;
+
+        private System.Windows.Forms.Label labelVelocity;
+        private System.Windows.Forms.Label labelGravity;
+        private System.Windows.Forms.Label labelDrag;
+        private System.Windows.Forms.Label labelTimeStep;
+        private System.Windows.Forms.Label labelSteps;
+
+        private System.Windows.Forms.TextBox txtVelocity;
+        private System.Windows.Forms.TextBox txtGravity;
+        private System.Windows.Forms.TextBox txtDrag;
+        private System.Windows.Forms.TextBox txtTimeStep;
+        private System.Windows.Forms.TextBox txtSteps;
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+        private System.Windows.Forms.PictureBox picGraph;
+    }
+}
---
+++ PARACHUTE MOTION SIMULATION/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PARACHUTE_MOTION_SIMULATION
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmp;
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGraph();
+            toolStripStatusLabel1.Text = "Ready";
+        }
+
+        private void InitializeGraph()
+        {
+            bmp = new Bitmap(
+                picGraph.Width,
+                picGraph.Height);
+
+            g = Graphics.FromImage(bmp);
+
+            picGraph.Image = bmp;
+
+            DrawGraph();
+        }
+
+        private void DrawGraph()
+        {
+            g.Clear(Color.White);
+
+            DrawGrid();
+            DrawAxes();
+
+            picGraph.Refresh();
+        }
+
+        // ================= GRID =================
+
+        private void DrawGrid()
+        {
+            int margin = 40;
+
+            using (Pen pen = new Pen(Color.LightGray))
+            {
+                pen.DashStyle =
+                    System.Drawing.Drawing2D.DashStyle.Dash;
+
+                for (int i = 0; i <= 20; i++)
+                {
+                    float x =
+                        margin +
+                        i * (picGraph.Width - 2 * margin) / 20f;
+
+                    g.DrawLine(
+                        pen,
+                        x,
+                        margin,
+                        x,
+                        picGraph.Height - margin);
+
+                    float y =
+                        margin +
+                        i * (picGraph.Height - 2 * margin) / 20f;
+
+                    g.DrawLine(
+                        pen,
+                        margin,
+                        y,
+                        picGraph.Width - margin,
+                        y);
+                }
+            }
+        }
+
+        // ================= AXES =================
+
+        private void DrawAxes()
+        {
+            int centerX = picGraph.Width / 2;
+            int centerY = picGraph.Height / 2;
+
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                // X Axis
+                g.DrawLine(
+                    pen,
+                    40,
+                    centerY,
+                    picGraph.Width - 40,
+                    centerY);
+
+                // Y Axis
+                g.DrawLine(
+                    pen,
+                    centerX,
+                    40,
+                    centerX,
+                    picGraph.Height - 40);
+            }
+
+            g.DrawString(
+                "Parachute Motion Simulation",
+                new Font("Segoe UI", 14, FontStyle.Bold),
+                Brushes.DarkBlue,
+                110,
+                10);
+
+            g.DrawString(
+                "Time (s)",
+                new Font("Segoe UI", 10),
+                Brushes.Black,
+                picGraph.Width - 80,
+                centerY + 10);
+
+            g.DrawString(
+                "Velocity (m/s)",
+                new Font("Segoe UI", 10),
+                Brushes.Black,
+                centerX + 10,
+                40);
+        }
+
+        // ================= PARACHUTE SIMULATION =================
+
+        private void RunSimulation()
+        {
+            if (!ReadInputs(
+                out double velocity,
+                out double gravity,
+                out double drag,
+                out double dt,
+                out int steps))
+                return;
+
+            DrawGraph();
+
+            float W = picGraph.Width / 2;
+            float H = picGraph.Height / 2;
+
+            float totalTime = steps * (float)dt;
+
+            float xScale =
+                (picGraph.Width - 80) / totalTime;
+
+            double[] time = new double[steps + 1];
+            double[] v = new double[steps + 1];
+
+            time[0] = 0;
+            v[0] = velocity;
+
+            // Find maximum velocity for graph scaling
+            double maxVelocity = Math.Abs(velocity);
+            double terminalVelocity = gravity / drag;
+            for (int i = 0; i < steps; i++)
+            {
+                v[i + 1] =
+                    v[i] +
+                    (gravity - drag * v[i]) * dt;
+
+                time[i + 1] =
+                    time[i] + dt;
+
+                if (Math.Abs(v[i + 1]) > maxVelocity)
+                    maxVelocity = Math.Abs(v[i + 1]);
+            }
+
+            if (maxVelocity < 1)
+                maxVelocity = 1;
+
+            float yScale =
+                (picGraph.Height - 80) /
+                (float)maxVelocity;
+
+            using (Pen pen = new Pen(Color.Blue, 3))
+            {
+                PointF previous =
+                    new PointF(
+                        W,
+                        H - (float)(Math.Abs(v[0]) * yScale));
+
+                for (int i = 1; i <= steps; i++)
+                {
+                    PointF current =
+                        new PointF(
+                            W + (float)(time[i] * xScale),
+                            H - (float)(Math.Abs(v[i]) * yScale)
+                        );
+
+
+                    g.DrawLine(
+                        pen,
+                        previous,
+                        current);
+
+
+                    g.FillEllipse(
+                        Brushes.Red,
+                        current.X - 3,
+                        current.Y - 3,
+                        6,
+                        6);
+
+
+                    previous = current;
+
+
+                    // Stop when terminal velocity is reached
+                    if (Math.Abs(v[i] - v[i - 1]) < 0.001)
+                        break;
+                }
+            }
+
+            picGraph.Refresh();
+
+            toolStripStatusLabel1.Text =
+$"Completed. Terminal velocity = {terminalVelocity:F2} m/s";
+        }
+
+        // ================= INPUT VALIDATION =================
+
+        private bool ReadInputs(
+            out double velocity,
+            out double gravity,
+            out double drag,
+            out double dt,
+            out int steps)
+        {
+            velocity = 0;
+            gravity = 0;
+            drag = 0;
+            dt = 0;
+            steps = 0;
+
+            if (!double.TryParse(txtVelocity.Text, out velocity) ||
+                !double.TryParse(txtGravity.Text, out gravity) ||
+                !double.TryParse(txtDrag.Text, out drag) ||
+                !double.TryParse(txtTimeStep.Text, out dt) ||
+                !int.TryParse(txtSteps.Text, out steps))
+            {
+                MessageBox.Show(
+                    "Please enter valid numeric values.",
+                    "Input Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return false;
+            }
+
+            if (gravity <= 0)
+            {
+                MessageBox.Show(
+                    "Gravity must be greater than zero.");
+                return false;
+            }
+
+            if (drag <= 0)
+            {
+                MessageBox.Show(
+                    "Drag coefficient must be greater than zero.");
+                return false;
+            }
+
+            if (dt <= 0)
+            {
+                MessageBox.Show(
+                    "Time step must be greater than zero.");
+                return false;
+            }
+
+            if (steps <= 0)
+            {
+                MessageBox.Show(
+                    "Number of steps must be greater than zero.");
+                return false;
+            }
+
+            return true;
+        }
+
+        // ================= CLEAR GRAPH =================
+
+        private void ClearGraph()
+        {
+            DrawGraph();
+
+            toolStripStatusLabel1.Text =
+                "Graph cleared.";
+        }
+
+        // ================= RESET PARAMETERS =================
+
+        private void ResetParameters()
+        {
+            txtVelocity.Text = "150";
+            txtGravity.Text = "9.8";
+            txtDrag.Text = "1";
+            txtTimeStep.Text = "0.1";
+            txtSteps.Text = "150";
+
+            ClearGraph();
+
+            toolStripStatusLabel1.Text =
+                "Parameters reset.";
+        }
+
+        // ================= SAVE GRAPH =================
+
+        private void SaveGraph()
+        {
+            try
+            {
+                string path =
+                    Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.Desktop),
+                        "ParachuteMotionGraph.png");
+
+                bmp.Save(
+                    path,
+                    ImageFormat.Png);
+
+                MessageBox.Show(
+                    "Graph saved successfully.\n\n" + path);
+
+                toolStripStatusLabel1.Text =
+                    "Graph saved.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // ================= EXPORT CSV =================
+
+        private void ExportCSV()
+        {
+            try
+            {
+                if (!ReadInputs(
+                    out double velocity,
+                    out double gravity,
+                    out double drag,
+                    out double dt,
+                    out int steps))
+                    return;
+
+                string path =
+                    Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.Desktop),
+                        "ParachuteMotionData.csv");
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("Step,Time(s),Velocity(m/s)");
+
+                    double t = 0;
+                    double v = velocity;
+
+                    sw.WriteLine($"0,{t:F2},{v:F4}");
+
+                    for (int i = 1; i <= steps; i++)
+                    {
+                        v = v + (gravity - drag * v) * dt;
+                        t += dt;
+
+                        sw.WriteLine(
+                            $"{i},{t:F2},{v:F4}");
+
+                        // Stop when terminal velocity is reached
+                        if (Math.Abs(
+                            gravity - drag * v) < 0.001)
+                            break;
+                    }
+                }
+
+                MessageBox.Show(
+                    "CSV exported successfully.\n\n" + path);
+
+                toolStripStatusLabel1.Text =
+                    "CSV exported.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // ================= TOOLSTRIP EVENTS =================
+
+        private void toolStripButton1_Click(
+            object sender,
+            EventArgs e)
+        {
+            RunSimulation();
+        }
+
+        private void toolStripButton2_Click(
+            object sender,
+            EventArgs e)
+        {
+            ClearGraph();
+        }
+
+        private void toolStripButton3_Click(
+            object sender,
+            EventArgs e)
+        {
+            ResetParameters();
+        }
+
+        private void toolStripButton4_Click(
+            object sender,
+            EventArgs e)
+        {
+            SaveGraph();
+        }
+
+        private void toolStripButton5_Click(
+            object sender,
+            EventArgs e)
+        {
+            ExportCSV();
+        }
+
+        // ================= MOUSE COORDINATES =================
+
+        private void picGraph_MouseMove(
+            object sender,
+            MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text =
+                $"X = {e.X}    Y = {e.Y}";
+        }
+
+  
+    }
+}
